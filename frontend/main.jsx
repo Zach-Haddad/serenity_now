2396c9925d8ddfe23e59003ff6bb482e8a1056c3
Zach-Haddad <Zhaddad7@gmail.com>
2017-05-22 11:15:58 -0700

    test cloudinary

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -3,9 +3,14 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root.jsx';
 
+const addAudio = function(path, idx){
+
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   const root = document.getElementById('root');
+  const cloud = 'http://res.cloudinary.com/zach/video/upload/';
 
   // setup all audio files here; implement load screen if streaming not seemless
   // connect all audio files to audiocontext, analyser
@@ -14,7 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const audios = document.getElementById('audios');
   const audio = document.createElement('audio');
   audio.id = "audio1";
-  audio.src = './assets/tracks/white-noise.mp3';
+  audio.src = cloud + 'v1495476734/white-noise_h3ti4t.mp3';
   audios.appendChild(audio);
 
   console.log(audio);
