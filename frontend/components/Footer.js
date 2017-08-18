02f32b00024368d674fd4fa2e54d74dcc25f7ce2
Zach-Haddad <Zhaddad7@gmail.com>
2017-08-18 16:19:41 -0700

    links reversed

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -8,11 +8,11 @@ const Footer = () => {
       <h2 id="credit">Created by Zach Haddad</h2>
 
       <div className="links">
-        <a href="https://github.com/Zach-Haddad" target="_blank">
+        <a href="https://www.linkedin.com/in/zach-haddad" target="_blank">
         <img src={cloud + 'v1485545222/linkedin_rxxvr3.png'} alt=""/>
         </a>
 
-        <a href="https://www.linkedin.com/in/zach-haddad" target="_blank">
+        <a href="https://github.com/Zach-Haddad" target="_blank">
         <img src={cloud + 'v1485545221/github_vibk1u.png'}alt=""/>
         </a>
       </div>
